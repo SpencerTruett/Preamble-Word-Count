fb15bd5c434b02c15fa7625f420efafef181518c
Spencer Truett <srtruett1993@gmail.com>
2021-04-07 10:47:35 -0500

    finished commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>"{{ preamble }}"</h2>
+    <button @click="countWords">Count Words</button>
+    <div>
+      <h4>Words that begin with "T": {{ startsWithT }}</h4>
+      <h4>Words that end with "E": {{ endWithE }}</h4>
+      <h4>Words that begin with "T" and end with "E": {{ both }}</h4>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from "./data";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      preamble: data.text,
+      startsWithT: 0,
+      endWithE: 0,
+      both: 0
+    };
+  },
+  methods: {
+    countWords() {
+      const wordsOnly = this.preamble
+        .replace(/[^a-zA-Z ]/g, "", "")
+        .toLowerCase();
+
+      const wordArray = wordsOnly.split(" ");
+
+      const tWords = [];
+      const eWords = [];
+      const tandeWords = [];
+
+      for (let i = 0; i < wordArray.length; i++) {
+        if (wordArray[i].startsWith("t")) {
+          tWords.push(wordArray[i]);
+        }
+        if (wordArray[i].endsWith("e")) {
+          eWords.push(wordArray[i]);
+        }
+        if (wordArray[i].startsWith("t") && wordArray[i].endsWith("e")) {
+          tandeWords.push(wordArray[i]);
+        }
+      }
+
+      this.startsWithT = tWords.length;
+      this.endWithE = eWords.length;
+      this.both = tandeWords.length;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
